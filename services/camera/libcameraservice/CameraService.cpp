caa842b28f293610cb95015b659feb7e144bde8c
nuclearmistake <nuclearmistake@gmail.com>
2014-01-11 16:31:40 -0500

    User-selectable camera click sound [3/3]: (stock(bloop)/realistic/disabled)
    Originally: Option to disable camera sounds (3/3) by Jonathan Steadman
    
    Change-Id: Iff4eb5ca7dd13038bf852a76d0c605814864497a
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -907,11 +907,11 @@ void CameraService::loadSound() {
     if (mSoundRef++) return;
 
     char value[PROPERTY_VALUE_MAX];
-    property_get("persist.camera.shutter.disable", value, "0");
-    int disableSound = atoi(value);
+    property_get("persist.sys.camera-sound", value, "1");
+    int selectedSound = atoi(value);
 
-    if(!disableSound) {
-        mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
+    if(selectedSound != 0) {
+        mSoundPlayer[SOUND_SHUTTER] = newMediaPlayer(selectedSound == 1 ? "/system/media/audio/ui/camera_click.ogg" : "/system/media/audio/ui/camera_click_realistic.ogg");
         mSoundPlayer[SOUND_RECORDING] = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
     }
     else {
