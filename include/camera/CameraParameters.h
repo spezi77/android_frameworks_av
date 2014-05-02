f42972e5c4a9e1feb9f681dd94b438ecae845ba6
xplodwild <me@xplod.fr>
2014-05-02 23:24:39 +0200

    CameraParameters: Add Oppo Find 7 specific parameters
    Change-Id: I7fe37fbe57d42c83f5e0467713ab961ac653cfbe
    
    Conflicts:
    	include/camera/CameraParameters.h

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -979,10 +979,6 @@ public:
     // other modes.
     static const char FOCUS_MODE_CONTINUOUS_PICTURE[];
 
-#ifdef OPPO_CAMERA_HARDWARE
-    static const char FOCUS_MODE_MANUAL_POSITION[];
-#endif
-
     // Values for light special effects
     // Low-light enhancement mode
     static const char LIGHTFX_LOWLIGHT[];
@@ -1006,6 +1002,10 @@ public:
     static const char PANORAMA_MODE_NOT_INPROGRESS[];
     static const char PANORAMA_MODE_INPROGRESS[];
 #endif
+    // Oppo Find 7 additionnal parameters
+    static const char FOCUS_MODE_MANUAL_POSITION[];
+    static const char WHITE_BALANCE_MANUAL_CCT[];
+
     // Normal focus mode. Applications should call
     // CameraHardwareInterface.autoFocus to start the focus in this mode.
     static const char FOCUS_MODE_NORMAL[];
