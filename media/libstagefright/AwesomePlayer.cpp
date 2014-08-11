f23265ea99bd35fb0dc3cc974f2de1cf9f56d65c
Zhou Song <zhous@codeaurora.org>
2014-08-11 18:22:40 +0800

    libstagefright: stop audio source before deleting AudioPlayer
    -audio source won't be stopped during AudioPlayer destruction
     due to start failed and AUDIOPLAYER_STARTED not set
    -handle audio source stop in AwesomePlayer if AUDIOPLAYER_STARTED
     not set
    
    Change-Id: I395c045a0a8a5d5b6ea8cc762d6a2636bb885b0e
    CRs-Fixed: 704605

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1080,6 +1080,10 @@ status_t AwesomePlayer::play_l() {
             }
 
             if (err != OK) {
+                if ((mAudioPlayer == NULL || !(mFlags & AUDIOPLAYER_STARTED))
+                        && mAudioSource != NULL) {
+                    mAudioSource->stop();
+                }
                 mAudioSource.clear();
                 mOmxSource.clear();
 
@@ -2340,6 +2344,10 @@ void AwesomePlayer::onVideoEvent() {
             ALOGE("Failed to fallback to SW decoder err = %d", err);
             notifyListener_l(MEDIA_ERROR, MEDIA_ERROR_UNKNOWN, err);
 
+            if ((mAudioPlayer == NULL || !(mFlags & AUDIOPLAYER_STARTED))
+                    && mAudioSource != NULL) {
+                mAudioSource->stop();
+            }
             mAudioSource.clear();
             mOmxSource.clear();
 
